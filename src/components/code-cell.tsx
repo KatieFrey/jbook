f1361a71a4185485de99699540a82487ede0635d
Katharine Francis <katie-francis@hotmail.com>
2021-09-29 13:17:50 -0700

    Draggable Resizing components and error handling

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler'
+import Resizeable from './resizable';
+
+const CodeCell = () => {
+  const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output.code);
+      setErr(output.err);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input])
+
+
+
+  return (
+    <Resizeable direction="vertical">
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row'}}>
+        <Resizeable direction='horizontal'>
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+            />
+        </Resizeable>
+        <Preview code={code} err={err}/>
+      </div>
+    </Resizeable>
+  );
+}
+
+export default CodeCell;
