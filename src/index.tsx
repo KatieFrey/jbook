7e3534e39029be4ffc15e1c700032fee33bb594d
Katharine Francis <katie-francis@hotmail.com>
2021-09-28 18:57:28 -0700

    added monaco editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import {useEffect, useState, useRef} from 'react'
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -20,7 +22,7 @@ const App = () => {
     startService();
   }, []);
 
-  const onClick = async (input: string) => {
+  const onClick = async () => {
     if(!ref.current){
       return;
     }
@@ -65,11 +67,13 @@ const App = () => {
 
   return (
     <div>
-      <textarea value={input} onChange={(e) => {
-        onClick(e.target.value)
-        setInput(e.target.value)}}></textarea>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+        />
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
-        {/* <button onClick={onClick}>Submit</button> */}
+        <button onClick={onClick}>Submit</button>
       </div>
       <iframe title="code preview" ref={iframeCode} sandbox="allow-scripts" srcDoc={htmlCode} />
     </div>
